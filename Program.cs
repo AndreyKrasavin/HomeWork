6e5e1cac69a393fcb60e5d6dd08db8dc73854abc
Moloko <moloko@Krasavin.Moloko>
2023-04-20 21:25:46 +0300

    sdsd

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿Console.WriteLine("input number: ");
-int num = Convert.ToInt32(Console.ReadLine());
+﻿Console.WriteLine("введите два числа");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2)
+{
+    System.Console.WriteLine($" {num1} больше {num2}");
+}
+if (num1 < num2)
+{
+    System.Console.WriteLine($" {num2} больше {num1}");
+}
