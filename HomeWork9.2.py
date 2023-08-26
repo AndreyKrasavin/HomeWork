855d197491009d6cb11bce21a1774bc4da83d03a
AndreyKrasavin <78152473+AndreyKrasavin@users.noreply.github.com>
2023-08-26 12:32:04 +0300

    Update HomeWork9.2.py

--- HomeWork9.2.py
+++ HomeWork9.2.py
@@ -1,1 +1,7 @@
 
+import pandas as pd
+
+df = pd.read_csv('california.csv')
+
+df[df['population'] <=500 ], df['median_house_value'] # наверное тупо указывать еще условие что от 0 и больше, ведь количество людей не может быть отрицательным значение
+df[df['households'].max() ], df['population'].min()
